6da611b1485bfbd767b15e8370df24e66d8b654b
Vinícius Vieira <vvm@cin.ufpe.br>
2025-06-11 21:24:12 -0300

    Add classe Camera

---
+++ src/Camera.cpp
@@ -0,0 +1,15 @@
+#ifndef CAMERAHEADER
+#define CAMERAHEADER
+#include "Point.cpp"
+#include "Vector.cpp"
+
+class camera {
+    private:
+        point location;
+        point pointingAt;
+        vetor vetorUp;
+        // Vetores U, W e V ortonormais (Ortogonais entre si e de norma/intensidade 1)
+
+};
+
+#endif
